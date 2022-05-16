54a5b8d0fb05db3acae3547654bc8541a0f65ea6
joye-seo <104261048+joye-seo@users.noreply.github.com>
2022-05-16 16:15:06 +0900

    2. 당첨번호 확인
    - 당첨번호 임시로 하드코딩 진행
    
     - 보너스 점수 없이 임시로 2등 없이 작업

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -32,7 +32,44 @@ public class MainDrive {
 
 		}
 
-		System.out.print("번호입력하세요 : ");
+		int[] winNumbers = new int[6];
+
+		// 임시로 로직 확인 용 하드코딩
+		winNumbers[0] = 10;
+		winNumbers[1] = 11;
+		winNumbers[2] = 20;
+		winNumbers[3] = 21;
+		winNumbers[4] = 30;
+		winNumbers[5] = 40;
+
+		// 당첨번호 랜덤으로 작성
+		
+		//몇등인지 맞추는 로
+
+		// 당첨번호 확인
+
+		int correctCount = 0;
+
+		for (int myNum : myNumbers) {
+			for (int winNum : winNumbers) {
+				if (myNum == winNum) {
+					correctCount++;
+				}
+			}
+		}
+
+		// 당첨등수 확인
+		if (correctCount == 6) {
+			System.out.println("1등 축");
+		} else if (correctCount == 5) {
+			System.out.println("3등 ");
+		} else if (correctCount == 4) {
+			System.out.println("4등 ");
+		} else if (correctCount == 3) {
+			System.out.println("5등 ");
+		} else {
+			System.out.println("탈락");
+		}
 	}
 
 }
